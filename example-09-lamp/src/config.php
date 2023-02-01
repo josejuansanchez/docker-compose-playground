4b36b5eae8a39929a833bf20bec1f3300e3bc6c7
José Juan Sánchez <josejuan.sanchez@gmail.com>
2023-02-01 23:09:15 +0100

    Added environment variables

--- example-09-lamp/src/config.php
+++ example-09-lamp/src/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'mysql');
-define('DB_NAME', 'lamp_db');
-define('DB_USER', 'lamp_user');
-define('DB_PASSWORD', 'lamp_password');
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 $mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
